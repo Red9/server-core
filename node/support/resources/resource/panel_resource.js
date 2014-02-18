9b0fde10003b6f7259e6983a26c54ce0063aa540
srlm <srlm@srlmproductions.com>
2014-02-17 22:37:58 -0500

    Updated panel resource to better handle uploads.

--- node/support/resources/resource/panel_resource.js
+++ node/support/resources/resource/panel_resource.js
@@ -1,6 +1,9 @@
 
 var validator = require('validator');
 var cassandraPanel = require('./../datasources/cassandra_panel');
+var datasetResource = require('./dataset_resource');
+var common = require('./../common');
+var moment = require('moment');
 
 /**
  * 
@@ -24,11 +27,63 @@ exports.getPanel = function(constraints, panelParameters,
 
 };
 
+
+exports.createTemporaryPanel = function(datasetId, callback){
+    datasetResource.getDatasets({id:datasetId}, function(datasetList){
+        // Verify that the dataset exists
+       if(datasetList.length !== 1){
+           callback('Incorrect number of datasets for id ' + datasetId + ': ' + datasetList.length);
+       } else{
+           // Add new panel key to dataset.
+           var temporaryId = common.generateUUID();
+           var alternatePanels = datasetList[0].alternatePanels;
+           alternatePanels.temporaryPanels.push({
+              id:temporaryId,
+              createTime: moment().valueOf(),
+              creator:'',
+              axes:[]
+           });
+           var updatedDataset = {alternatePanels:alternatePanels};
+           datasetResource.updateDataset(datasetId, updatedDataset, function(err, changes){
+               if(err){
+                   callback('Could not update dataset');
+               }else{
+                   callback(undefined, temporaryId);
+               }
+           });
+       }
+    });
+};
+
+/**
+ * 
+ * @param {type} panelId the actual raw data panel ID (not dataset ID)
+ * @param {type} callback
+ * @returns {undefined} */
 exports.deletePanel = function(panelId, callback){
   cassandraPanel.deletePanel(panelId, callback);  
 };
 
-
+/**
+ * 
+ * @param {type} panelId the actual raw data panel ID (not dataset ID)
+ * @param {type} callback ({ } ) Calls with an object with the calculated properties.
+ * @returns {undefined}
+ */
 exports.calculatePanelProperties = function(panelId, callback){
     cassandraPanel.calculatePanelProperties(panelId, callback);  
 };
+
+
+/**
+ * 
+ * @param {type} panelId
+ * @param {type} time in MS since epoch
+ * @param {type} row list of float values
+ * @param {type} callback
+ * @returns {undefined}
+ */
+exports.addRows = function(panelId, rows, callback){
+   cassandraPanel.addRows(panelId, rows, callback); 
+};
+
