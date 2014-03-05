bae3fe0762c87dc7b2abe0771aa3c007d64cef9b
srlm <srlm@srlmproductions.com>
2014-03-05 12:49:01 -0500

    Added API to get list of users. Allows DW-103 to go ahead.

--- node/api/routes/user.js
+++ node/api/routes/user.js
@@ -1,21 +1,53 @@
+var userResource = requireFromRoot('support/resources/user');
+
+function simplifyOutput(eventArray) {
+    underscore.each(eventArray, function(element, index, list) {
+        // Nothing to simplify, for now.
+    });
+    return eventArray;
+}
 
 
-exports.search = function(req, res, next){
+exports.search = function(req, res, next) {
+    var simpleOutput = false;
+    if (typeof req.query['simpleoutput'] !== 'undefined') {
+        delete req.query['simpleoutput'];
+        simpleOutput = true;
+    }
+
+    // At this point, req.query has constraints.
+    userResource.getUsers(req.query, function(results) {
+        if (simpleOutput) {
+            results = simplifyOutput(results);
+        }
+        res.json(results);
+    });
+};
+
+exports.get = function(req, res, next) {
+    var simpleOutput = false;
+    if (typeof req.query['simpleoutput'] !== 'undefined') {
+        delete req.query['simpleoutput'];
+        simpleOutput = true;
+    }
+
+    // At this point, req.query has constraints.
+    userResource.getUsers({id:req.param('id')}, function(results) {
+        if (simpleOutput) {
+            results = simplifyOutput(results);
+        }
+        res.json(results);
+    });
+};
+
+exports.create = function(req, res, next) {
     res.status(501).json(JSON.parse('{"message":"Function not implemented yet."}'));
 };
 
-exports.get = function(req, res, next){
+exports.update = function(req, res, next) {
     res.status(501).json(JSON.parse('{"message":"Function not implemented yet."}'));
 };
 
-exports.create = function(req, res, next){
-    res.status(501).json(JSON.parse('{"message":"Function not implemented yet."}'));
-};
-
-exports.update = function(req, res, next){
-    res.status(501).json(JSON.parse('{"message":"Function not implemented yet."}'));
-};
-
-exports.delete = function(req, res, next){
+exports.delete = function(req, res, next) {
     res.status(501).json(JSON.parse('{"message":"Function not implemented yet."}'));
 };
