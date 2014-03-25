774647914169bd9d9aba1c2f09124e89ed1c264a
srlm <srlm@srlmproductions.com>
2014-03-25 14:55:47 -0400

    Finised up on first pass of SPA. It now has all the features of the old, non-SPA datasetdetails page. Finishes DW-118.

---
+++ development/js/tiles/resourcedetails.js
@@ -0,0 +1,35 @@
+function resourceDetails(myPlace, configuration, doneCallback) {
+    this.myPlace = myPlace;
+    $(sandbox).on('totalState.resource-focused', $.proxy(this.resourceFocused, this));
+
+    this.setResource('dataset', {});
+
+    doneCallback();
+}
+
+resourceDetails.prototype.setResource = function(type, resource) {
+
+    var details = {
+        apiUrl: sandbox.apiUrl
+    };
+
+    var self = this;
+    if (type === 'dataset') {
+        sandbox.requestTemplate('datasetdetails', function(template) {
+            details.dataset = resource;
+            self.myPlace.html(template(details));
+        });
+    }
+};
+
+resourceDetails.prototype.resourceFocused = function(event, parameters) {
+    if (parameters.type === 'dataset') {
+        this.setResource('dataset', parameters.resource);
+    } else if (parameters.type === 'event') {
+        // TODO(SRLM): Check to make sure that the event is still in the same
+        // dataset. If it's not then we probably need to remove the details or 
+        // update them. This could be an issue for when we allow changing events
+        // and go to a "new page".
+
+    }
+};
