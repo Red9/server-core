12865aa37e12620bb4f7d3139a56f54350eb4e63
srlm <srlm@srlmproductions.com>
2014-06-05 12:02:31 -0400

    A few minor changes for usability/bug fixes.

--- development/js/utilities/customHandlebarsHelpers.js
+++ development/js/utilities/customHandlebarsHelpers.js
@@ -76,6 +76,7 @@
             }
         }
         function percentFormater(numerator, denominator) {
+            console.log('numerator: ' + numerator + ', denominator: ' + denominator);
             if (isNaN(numerator) && isNaN(denominator)) {
                 return '---';
             } else if (isNaN(numerator)) {
