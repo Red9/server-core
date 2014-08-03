5e91a5c73f848bcd7d348056977bef698d5b870a
srlm <srlm@srlmproductions.com>
2014-08-03 18:32:41 -0400

    DW-163, DW-186, DW-195. Trying to get our upload process to the correct amount of robustness.

--- node/support/datasources/panelprocessors/distribution.js
+++ node/support/datasources/panelprocessors/distribution.js
@@ -3,6 +3,18 @@
 var underscore = require('underscore')._;
 var log = requireFromRoot('support/logger').log;
 
+
+/** Create a distribution based on the input rows.
+ * 
+ * @param {type} name       A name for this distribution.
+ * @param {type} valueIndex The index in the row array that the histogram values should be taken from.
+ * @param {type} minimum    The minimum value that the input is expected to be.
+ * @param {type} maximum    The maximum value that the input is expected to be.
+ * @param {type} slots      The number of buckets to fit the distribution into. Has all the tradeoffs that a traditional histogram has.
+ * @returns {object} with two functions:
+ *                        processRow(time, values) time is ignored, values is the array of values at that time. valueIndex selects from this array.
+ *                        proccessEnd() Returns the final distribution.
+ */
 exports.new = function(name, valueIndex, minimum, maximum, slots) {
     var distribution = [];
 
@@ -16,16 +28,24 @@ exports.new = function(name, valueIndex, minimum, maximum, slots) {
     }
 
     function processRow(time, values) {
+        // Sometimes one of the values is a NaN. While not a good thing (it
+        // probably indicates a problem in the underlaying data processing) we
+        // shouldn't have a problem ignoring it at this level.
+        if(underscore.isNaN(values[valueIndex])){
+            return;
+        }
+        
         var index = (values[valueIndex] - minimum) / (maximum - minimum) * (slots - 1);
         index = Math.floor(index);
         if (underscore.isNaN(values[valueIndex])) {
-            console.log('values[valueIndex]: ' + values[valueIndex] + ', index: ' + index + ', slots: ' + slots);
+            log.debug('values[valueIndex]: ' + values[valueIndex] + ', index: ' + index + ', slots: ' + slots + ', time: ' + time);
         }
 
         if (underscore.isNaN(index) || index < 0 || index >= slots) {
-            log.error('Wrong index: ' + index + ', value: ' + values[valueIndex]);
+            log.error('Wrong index: ' + index + ', value: ' + values[valueIndex] + ', time: ' + time);
+        } else {
+            distribution[index].count += 1;
         }
-        distribution[index].count += 1;
     }
 
 
