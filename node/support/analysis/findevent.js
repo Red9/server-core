7c9f6f1b08c4f826e9ea3354f42e9f9835b51bd5
srlm <srlm@srlmproductions.com>
2014-08-05 17:49:14 -0400

    DW-182: added life cycle support for event finding sessions. Now, event sessions will timeout after a certain number of hours, or will be killed if a new session (above a set limit of sessions) is started. Probably needs more testing, but good first pass.

--- node/support/analysis/findevent.js
+++ node/support/analysis/findevent.js
@@ -59,7 +59,7 @@ exports.random = function(datasetId, eventType, quantity) {
 };
 
 
-function createListener(lineCallback) {
+function createStreamToLine(lineCallback) {
     var updateBuffer = '';
     return function(something) {
         updateBuffer += something;
@@ -75,111 +75,206 @@ function createListener(lineCallback) {
     };
 }
 
+function startSession(datasetId, readyCallback) {
+    var broadcastId = 'sessionScript' + useful.generateInt(0, 999);
+    var script = spawn('Rscript', ['main.r'], {cwd: 'bin/eventfinder/'});
+    script.stdout.setEncoding('utf8');
+    script.stderr.setEncoding('utf8');
+    script.stdin.setEncoding('utf8');
 
-exports.session = function(datasetId, options) {
-    var broadcastId = 'session' + useful.generateInt(0, 999);
+    // Set up listeners for the script outputs.
+    // STDERR is user facing messages (what's happening)
+    // STDOUT is the result (one line of results for each line of commands).
+    var resultOutput = function() {
+    };
+
+    var stderrListener = createStreamToLine(function(line) {
+        sockets.broadcastStatus(broadcastId, line);
+    });
+
+    var stdoutListener = createStreamToLine(function(line) {
+        resultOutput(JSON.parse(line));
+    });
+
+    script.stderr.on('data', stderrListener);
+    script.stdout.on('data', stdoutListener);
+
+
+    var shutdownPeriod = 1000 * 60 * 60 * 1; // 2 hours
+    var shutdownTimer;
+
+    script.on('exit', function(code, signal) {
+        log.info('Script ' + datasetId + ' has exited.');
+        // Make sure that nobody else tries to use this.
+        delete sessionList[datasetId];
+
+        // Let's make sure to log the error code
+        if (code !== 0) {
+            var errorMessage = 'Non zero code! ' + code + '(' + signal + ')';
+            log.error(errorMessage);
+            sockets.broadcastStatus(broadcastId, errorMessage);
+        }
+
+        // Let's see if we can get more info. Reading stderr might lead to ECONNRESET, so beware.
+        var processingInfo = script.stderr.read();
+        if (code !== 0) {
+            var errorMessage = 'Non zero code! ' + code + '(' + signal + '): ' + processingInfo;
+            log.error(errorMessage);
+            sockets.broadcastStatus(broadcastId, errorMessage);
+            return;
+        }
+
+        // Clean up the last bit of output (or output blank line).
+        stderrListener('\n');
+        sockets.broadcastStatus(broadcastId, 'Script has been killed. Exiting now.');
+    });
+
+    var rowsOutput = 0;
+    // We've set everything up, so now we need to stream the panel to the script.
     datasetResource.get({id: datasetId}, function(datasetList) {
         if (datasetList.length !== 1) {
             log.error('Invalid dataset id: ' + datasetId);
             return;
         }
         var dataset = datasetList[0];
-
-        var start = new Date().getTime();
-        var script = spawn('Rscript', ['main.r'], {cwd: 'bin/eventfinder/'});
-        script.stdout.setEncoding('utf8');
-        script.stderr.setEncoding('utf8');
-        script.stdin.setEncoding('utf8');
-
-        // Read this as we go along so that the user can get live updates.
-        var updateListener = createListener(function(line) {
-            sockets.broadcastStatus(broadcastId, line);
-        });
-        script.stderr.on('data', updateListener);
-
-        // clone so we have an "original" copy for our source storage.
-        var storedOptions = _.clone(options);
-
-        script.on('exit', function(code, signal) {
-
-            // Let's make sure to log the error code
-            if (code !== 0) {
-                var errorMessage = 'Non zero code! ' + code + '(' + signal + ')';
-                log.error(errorMessage);
-                sockets.broadcastStatus(broadcastId, errorMessage);
-            }
-
-            // Let's see if we can get more info. Reading stderr might lead to ECONNRESET, so beware.
-            var processingInfo = script.stderr.read();
-            if (code !== 0) {
-                var errorMessage = 'Non zero code! ' + code + '(' + signal + '): ' + processingInfo;
-                log.error(errorMessage);
-                sockets.broadcastStatus(broadcastId, errorMessage);
-                return;
-            }
-
-            // Clean up the last bit of output (or output blank line).
-            updateListener('\n');
-
-            // Record the amount of time for execution
-            var executionSeconds = ((new Date().getTime()) - start) / 1000;
-            sockets.broadcastStatus(broadcastId, 'Execution time: ' + executionSeconds + ' seconds');
-
-            // The found events
-            var output = JSON.parse(script.stdout.read());
-            var events = output.results;
-
-            // Let the user know.
-            sockets.broadcastStatus(broadcastId, '--- Found ' + events.length + ' events ---');
-            sockets.broadcastStatus(broadcastId, 'Calculating summary statistics for detected events.');
-            sockets.broadcastStatus(broadcastId, 'Please give it a minute and then refresh the page to see your new events.');
-
-            // Now we need to actually store all the events.
-            _.each(events, function(event) {
-                event.datasetId = datasetId;
-                event.source = {
-                    type: 'auto',
-                    algorithm: 'eventdetection',
-                    parameters: storedOptions
-                };
-
-                eventResource.create(event, function(err) {
-                    if (err) {
-                        log.error('Could not create event: ' + err);
-                    }
-                });
-            });
-
-        });
-
-        var rowsOutput = 0;
-        // We've set everything up, so now we need to stream the panel to the script.
         panelResource.getPanelBody({
             id: dataset.headPanelId
         }, function(panel) {
-            var axisList = _.reduce(panel.axes, function(memo, column, index) {
-                return memo + ',' + column;
-            }, 'time');
-
+            // Output the number of rows so that the script can prepare
             sockets.broadcastStatus(broadcastId, 'Need to load ' + panel.rowCount + ' rows of data.');
             script.stdin.write(JSON.stringify({
                 rowCount: panel.rowCount
             }) + '\n');
 
+            // Output the axes (column titles) as a CSV list
+            var axisList = _.reduce(panel.axes, function(memo, column, index) {
+                return memo + ',' + column;
+            }, 'time');
             script.stdin.write(axisList + '\n');
         }, function(time, data, index) {
+            // Output the data row as a CSV.
             var row = _.reduce(data, function(memo, value) {
                 return memo + ',' + value;
             }, time);
             script.stdin.write(row + '\n');
             rowsOutput++;
         }, function(err) {
-            var t = JSON.stringify(options) + '\n'
-                    + JSON.stringify({
-                        command: 'exit'
-                    }) + '\n';
-            script.stdin.write(t);
+            shutdownTimer = setTimeout(shutdown, shutdownPeriod);
+            readyCallback();
         });
+    });
+
+    function sendCommand(options, resultCallback) {
+        clearTimeout(shutdownTimer);
+
+        // If the script exits now that means it has crashed during processing,
+        // which we don't want but must accomodate by not leaving our listerener
+        // hanging.
+        var exitFunction = function() {
+            resultCallback('Premature exit: script crash.');
+        };
+        script.on('exit', exitFunction);
+
+        resultOutput = function(result) {
+            resultCallback(undefined, result);
+            script.removeListener('exit', exitFunction);
+            shutdownTimer = setTimeout(shutdown, shutdownPeriod);
+        };
+
+        script.stdin.write(JSON.stringify(options) + '\n');
+    }
+    function shutdown() {
+        log.info('Shutting down script for dataset ' + datasetId);
+        var t = JSON.stringify({
+            command: 'exit'
+        }) + '\n';
+        script.stdin.write(t);
+    }
+
+    return {
+        sendCommand: sendCommand,
+        shutdown: shutdown
+    };
+}
+
+var maximumSessions = 2;
+var sessionList = {};
+
+function getSession(datasetId, callback) {
+    if (_.has(sessionList, datasetId)) {
+        // Update the access time to now
+        sessionList[datasetId].time = (new Date()).getTime();
+        callback(sessionList[datasetId].session);
+    } else {
+        // Need to create a new session
+        var createdSession = startSession(datasetId,
+                function() {
+                    sessionList[datasetId] = {
+                        session: createdSession,
+                        time: (new Date()).getTime()
+                    }
+                    callback(createdSession);
+
+                    // And trim the size of the list.
+                    while (_.size(sessionList) > maximumSessions) {
+                        var oldestDataset = _.reduce(sessionList,
+                                function(memo, current, datasetId) {
+                                    console.log('datasetId: ' + datasetId + ', memo: ' + JSON.stringify(memo));
+                                    if (typeof memo === 'undefined'
+                                            || current.time < sessionList[memo].time) {
+                                        return datasetId;
+                                    } else {
+                                        return memo;
+                                    }
+                                }, undefined);
+                        log.info('sessionList too big: removing oldest dataset (' + oldestDataset + ')');
+                        sessionList[oldestDataset].session.shutdown();
+                        delete sessionList[oldestDataset];
+                    }
+                });
+    }
+}
+
+function runOption(datasetId, options, session) {
+    var broadcastId = 'session' + useful.generateInt(0, 999);
+    sockets.broadcastStatus(broadcastId, 'Starting command "' + options.command + '"');
+    session.sendCommand(options,
+            function(err, result) {
+                if (err) {
+                    sockets.broadcastStatus('Error: ' + err);
+                    return;
+                }
+                // The found events
+
+                var events = result.results;
+
+                // Let the user know.
+                sockets.broadcastStatus(broadcastId, '--- Found ' + events.length + ' events ---');
+                sockets.broadcastStatus(broadcastId, 'Calculating summary statistics for detected events.');
+                sockets.broadcastStatus(broadcastId, 'Please give it a minute and then refresh the page to see your new events.');
+
+                // Now we need to actually store all the events.
+                _.each(events, function(event) {
+                    event.datasetId = datasetId;
+                    event.source = {
+                        type: 'auto',
+                        algorithm: 'eventdetection',
+                        parameters: options
+                    };
+
+                    eventResource.create(event, function(err) {
+                        if (err) {
+                            log.error('Could not create event: ' + err);
+                        }
+                    });
+                });
+            }
+    );
+}
+
+exports.session = function(datasetId, options) {
+    getSession(datasetId, function(session) {
+        runOption(datasetId, options, session);
     });
 };
 
@@ -291,9 +386,6 @@ exports.spectral = function(options) {
         }, function(err) {
             script.stdin.write('\n\n');
         });
-
-
-
     });
 
 };
