bef6a04c4493d1d0d65f7782d4637ebe4b85b9eb
srlm <srlm@srlmproductions.com>
2014-08-10 16:57:19 -0400

    DW-202: Added more statistics to display for the dataset. Now need to change the event statistics display.

--- development/js/tiles/resourcedetails.js
+++ development/js/tiles/resourcedetails.js
@@ -1,4 +1,4 @@
-define(['vendor/jquery', 'utilities/commentList'], function($) {
+define(['vendor/jquery', 'vendor/underscore', 'utilities/commentList'], function($, _) {
     function resourceDetails(sandbox, tile, configuration, doneCallback) {
         function init() {
             tile.setTitle('details');
@@ -14,6 +14,37 @@ define(['vendor/jquery', 'utilities/commentList'], function($) {
             doneCallback();
         }
 
+        function calculateAggregatedEvents(datasetId, callback) {
+            if (typeof datasetId === 'undefined') {
+                callback();
+                return;
+            }
+            sandbox.get('dataset', {id: datasetId}, function(datasetList) {
+                var dataset = datasetList[0];
+                var datasetDuration = dataset.headPanel.endTime - dataset.headPanel.startTime;
+                sandbox.get('event', {datasetId: datasetId}, function(events) {
+                    callback(_.sortBy(_.map(_.toArray(_.reduce(events, function(memo, event) {
+                        if (_.has(memo, event.type) === false) {
+                            memo[event.type] = {
+                                type: event.type,
+                                count: 0,
+                                duration: 0
+                            };
+                        }
+                        memo[event.type].count += 1;
+                        memo[event.type].duration += (event.endTime - event.startTime);
+                        return memo;
+                    }, {})), function(aggregatedEvent) {
+                        aggregatedEvent.durationPercent = aggregatedEvent.duration / datasetDuration;
+                        return aggregatedEvent;
+                    }), function(aggregatedEvent) {
+                        return aggregatedEvent.type;
+                    }));
+                });
+            }, ['headPanel', 'owner']);
+
+        }
+
         function setResource(type, id) {
             var expand = type === 'dataset' ? ['headPanel', 'owner'] : undefined;
             sandbox.get(type, {id: id}, function(resourceList) {
@@ -24,8 +55,17 @@ define(['vendor/jquery', 'utilities/commentList'], function($) {
                         type: type.charAt(0).toUpperCase() + type.slice(1)
                     };
                     parameters[type] = resource;
-                    
-                    tile.place.html(template(parameters));
+
+                    if (type === 'dataset') {
+                        calculateAggregatedEvents(id, function(aggregation) {
+                            parameters.aggregation = aggregation;
+                            tile.place.html(template(parameters));
+                        });
+                    } else if (type === 'event') {
+                        tile.place.html(template(parameters));
+                    } else {
+                        tile.place.html(template(parameters));
+                    }
                 });
             }, expand);
         }
