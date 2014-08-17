af437ba9f8cfafe8675b4b09638f58cab8ed033d
srlm <srlm@srlmproductions.com>
2014-08-17 12:04:17 -0400

    More work on DW-134. At this point it's transformed into converting the whole site (minus graph pages) to Angular. Still rough, but we have a good framework at this point.

--- development/js/singlepage/app.js
+++ development/js/singlepage/app.js
@@ -2,14 +2,56 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+    'ui.bootstrap',
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/dataset/', {templateUrl: '/partials/searchdataset.html', controller: 'searchDataset'});
-  $routeProvider.when('/event/', {templateUrl: '/partials/searchevent.html', controller: 'searchEvent'});
-  $routeProvider.otherwise({redirectTo: '/dataset/'});
-}]);
+        config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+                $routeProvider.when('/', {
+                    templateUrl: '/partials/home.html',
+                    controller: 'homeController'
+                });
+                $routeProvider.when('/dataset/', {
+                    templateUrl: '/partials/searchdataset.html',
+                    controller: 'searchDataset',
+                    reloadOnSearch: false
+                });
+                $routeProvider.when('/event/', {
+                    templateUrl: '/partials/searchevent.html',
+                    controller: 'searchEvent'
+                });
+                $routeProvider.when('/about', {
+                    templateUrl: '/partials/about.html'
+                });
+                $routeProvider.when('/login', {
+                    templateUrl: '/partials/login.html'
+                });
+                $routeProvider.when('/user/me', {
+                    templateUrl: '/partials/myprofile.html',
+                    controller: 'myProfile'
+                });
+                $routeProvider.when('/user/:id', {
+                    templateUrl: '/partials/userprofile.html',
+                    controller: 'userProfile'
+                });
+                $routeProvider.when('/404', {
+                    templateUrl: '/partials/404.html'
+                });
+                $routeProvider.when('/monitor', {
+                    templateUrl: '/partials/monitor.html'
+                });
+
+                // /logout
+                $routeProvider.otherwise({
+                    redirectTo: '/404'
+                });
+
+
+
+                $locationProvider.html5Mode(true);
+            }]);
+
+
