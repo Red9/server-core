2428189ade7305f1e4839e2a1834606ae24e8191
srlm <srlm@srlmproductions.com>
2014-08-18 18:58:47 -0400

    Worked out all the login details, and the page navigation flow. Getting better at Angular.

--- development/js/singlepage/controllers.js
+++ development/js/singlepage/controllers.js
@@ -3,81 +3,57 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-        .controller('pageController', ['$scope', 'currentUser', function($scope, currentUser) {
-                console.log('new page!');
-                $scope.currentUser = currentUser.getCurrentUser();
-                $scope.site = {
-                    title: 'test site',
-                    description: 'a cool new version',
-                    author: {
-                        displayName: 'srlm',
-                        email: 'srlm@srlmproductions.com'
-                    }
+        .controller('pageController', function($scope, $location, $cookieStore) {
+            $scope.logout = function() {
+                $scope.currentUser = null;
+                $cookieStore.remove('connect.sid');
+                $location.path('/page/about');
+            };
+        })
+        .controller('searchDataset', function($scope, datastore) {
+            $scope.searchFilters = {};
+            $scope.datasetList = null;
+
+            $scope.$watch('searchFilters', function(newValue, oldValue) {
+                var parameters = {
+                    part: 'title,id,createTime,headPanel.startTime,headPanel.endTime,owner.id,owner.displayName,count',
+                    count: true,
+                    expand: 'headPanel,owner'
+                };
+                $scope.datasetList = null;  // Clear variable to indicate loading
+                datastore.get('dataset', angular.extend(parameters, newValue), function(data) {
+                    $scope.datasetList = data;
+                });
+
+            });
+        })
+        .controller('searchEvent', function($scope, datastore) {
+            $scope.searchFilters = {};
+
+            $scope.eventList = null;
+
+            $scope.$watch('searchFilters', function(newValue, oldValue) {
+                var parameters = {
+                    part: 'type,id,startTime,endTime'
                 };
 
-                $scope.$on('currentUser', function(event, newUser) {
-                    $scope.currentUser = currentUser.getCurrentUser();
+                $scope.eventList = null; // Clear variable to indicate loading
+                datastore.get('event', angular.extend(parameters, newValue), function(data) {
+                    $scope.eventList = data;
                 });
 
-            }])
-        .controller('searchDataset', ['$scope', 'datastore', function($scope, datastore) {
-                $scope.searchFilters = {
-                    'owner.id': 'fadedcab-0528-e834-b47d-c80e3696846c'
-                };
-
-                $scope.datasetList = [];
-
-                $scope.$on('$routeUpdate', function() {
-                    console.log('route has been updated.');
-                });
-
-                $scope.$watch('searchFilters', function(newValue, oldValue) {
-                    console.log('search has changed: ' + JSON.stringify(newValue));
-                    var parameters = {
-                        part: 'title,id,createTime,headPanel.startTime,headPanel.endTime,owner.id,owner.displayName,count',
-                        count: true,
-                        expand: 'headPanel,owner'
-                    };
-
-                    datastore.get('dataset', angular.extend(parameters, newValue), function(data) {
-                        console.log('got results');
-                        $scope.datasetList = data;
+            });
+        })
+        .controller('myProfile',
+                function($scope) {
+                })
+        .controller('userProfile',
+                function($scope, $routeParams, datastore) {
+                    datastore.get('user', {id: $routeParams['id']}, function(data) {
+                        $scope.user = data[0];
                     });
-
+                })
+        .controller('homeController',
+                function($scope) {
                 });
-            }])
-        .controller('searchEvent', ['$scope', 'datastore', function($scope, datastore) {
-                $scope.searchFilters = {};
-
-                $scope.eventList = [];
-
-                $scope.$on('$routeUpdate', function() {
-                    console.log('route has been updated.');
-                });
-
-                $scope.$watch('searchFilters', function(newValue, oldValue) {
-                    console.log('search has changed: ' + JSON.stringify(newValue));
-                    var parameters = {
-                        part: 'type,id,startTime,endTime'
-                    };
-
-                    datastore.get('event', angular.extend(parameters, newValue), function(data) {
-                        console.log('got results');
-                        $scope.eventList = data;
-                    });
-
-                });
-            }])
-        .controller('myProfile', ['$scope', 'currentUser', 'datastore',
-            function($scope, currentUser, datastore) {
-                $scope.user = currentUser.getCurrentUser();
-            }])
-        .controller('userProfile', ['$scope',
-            function($scope) {
-
-            }])
-        .controller('homeController', ['$scope',
-            function($scope) {
-
-            }]);
                
--- development/js/singlepage/services.js
+++ development/js/singlepage/services.js
@@ -7,45 +7,28 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
         .value('version', '0.1')
-        .factory('currentUser', ['$http', '$rootScope', function($http, $rootScope) {
-                var apiUrl = 'http://api.localdev.redninesensor.com/';
-                var currentUser = null;
-
-                $http({
-                    method: 'GET',
-                    withCredentials: true,
-                    url: apiUrl + 'user/current'
-                }).success(function(data, status) {
-                    currentUser = data;
-                    $rootScope.$broadcast('currentUser', currentUser);
-                }).error(function(data, status) {
-                    currentUser = null;
-                    $rootScope.$broadcast('currentUser', {user: null});
-                });
-
-                return {
-                    getCurrentUser: function() {
-                        return currentUser;
-                    }
-                };
-            }])
-        .factory('datastore', ['$http', function($http) {
-                var apiUrl = 'http://api.localdev.redninesensor.com/';
-
+        .factory('datastore', ['$http', 'apiUrl', function($http, apiUrl) {
                 return {
                     get: function(resourceName, parameters, callback) {
                         $http({
                             method: 'GET',
                             params: parameters,
                             withCredentials: true,
-                            url: apiUrl + resourceName + '/'
+                            url: apiUrl + '/' + resourceName + '/'
                         }).success(function(data, status) {
                             callback(data);
                         }).error(function(data, status) {
                             console.log('Error!');
                         });
                     }
-                }
-            }]);
-
- 
+                };
+            }])
+        .factory('api', function($resource, apiUrl) {
+            return {
+                dataset: $resource(apiUrl + '/dataset/:id', {id: '@id'}),
+                event: $resource(apiUrl + '/event/:id', {id: '@id'}),
+                comment: $resource(apiUrl + '/comment/:id', {id: '@id'}),
+                user: $resource(apiUrl + '/user/:id', {id: '@id'}),
+                video: $resource(apiUrl + '/video/:id', {id: '@id'})
+            };
+        });
