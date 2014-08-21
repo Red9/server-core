3e0e2b8ebd9bd4db796af0f7bc13983ab75ec019
srlm <srlm@srlmproductions.com>
2014-08-20 21:23:01 -0400

    DW-134: All done moving the home page to Angular, at least for now. Still need to get the HTML server refactored.

--- development/js/singlepage/directives.js
+++ development/js/singlepage/directives.js
@@ -1,84 +1,187 @@
 'use strict';
 
 /* Directives */
-angular.module('myApp.directives', [])
-        .directive('appVersion', ['version', function(version) {
-                return function(scope, elm, attrs) {
-                    elm.text(version);
-                };
-            }])
-        .directive('datasetList', ['datastore', function(datastore) {
-                return {
-                    restrict: 'E',
-                    templateUrl: '/partials/datasetlist.html',
-                    scope: {
-                        datasetList: '='
-                    },
-                    controller: function($scope, _) {
-                        $scope.deleteSelected = function(list) {
-                            var selectedList = _.filter(list, function(resource){
-                                return resource.selected;
-                            });
-                            
-                            console.log('selectedList: ' + JSON.stringify(selectedList));
-                        };
-                    },
-                    link: function(scope, elem, attrs) {
-                        console.log('datasetList');
+angular.module('redApp.directives', [])
+        .directive('resourceList', function() {
+            return {
+                restrict: 'E',
+                templateUrl: function(element, attributes) {
+                    return '/partials/' + attributes.resourceType + 'list.html';
+                },
+                scope: {
+                    resourceFilters: '=',
+                    resourceType: '@'
+                },
+                controller: function($scope, $location, $q, _, api, confirmDialog) {
+                    $scope.resourceList = null;
+
+                    var parameters = {
+                        dataset: {
+                            part: 'title,id,createTime,headPanel.startTime,headPanel.endTime,owner.id,owner.displayName,count',
+                            count: true,
+                            expand: 'headPanel,owner'
+                        },
+                        event: {
+                            part: 'type,id,startTime,endTime,datasetId'
+                        }
+
+                    };
+
+                    var sortBy = {
+                        dataset: function(dataset) {
+                            return -dataset.createTime;
+                        },
+                        event: function(event) {
+                            return -event.startTime;
+                        }
+                    };
+
+
+                    // Creates a page of resources suitable for display. This
+                    // page is just a shallow copy, so no need to use it for
+                    // anything but display.
+                    function extractResourcePage() {
+
+                        if ($scope.resourceList) {
+                            var page = $scope.resourcePages.currentPage;
+                            var ipp = $scope.resourcePages.itemsPerPage;
+
+                            $scope.resourcePage = $scope.resourceList.slice(
+                                    (page - 1) * ipp,
+                                    page * ipp
+                                    );
+                        } else {
+                            $scope.resourcePage = null;
+                        }
                     }
-                };
-            }])
-        .directive('datasetSearch', ['datastore', function(datastore) {
-                return {
-                    restrict: 'E',
-                    scope: {
-                        searchFilters: '='
-                    },
-                    templateUrl: '/partials/datasetsearch.html',
-                    controller: function($scope) {
-                        $scope.formChange = function(searchForm) {
 
-                            // title, user, startTime, endTime
+                    $scope.$watch('resourcePages.currentPage', extractResourcePage);
+                    $scope.$watch('resourcePages.itemsPerPage', extractResourcePage);
+                    $scope.$watchCollection('resourceList', extractResourcePage);
 
-                            function isValidInput(input) {
-                                return angular.isDefined(input)
-                                        && input !== null
-                                        && input !== ''
-                                        && input !== {};
-                            }
+                    $scope.$watch('resourceFilters', function(newValue, oldValue) {
+                        $scope.resourceList = null;  // Clear variable to indicate loading
 
-                            var search = {};
+                        api[$scope.resourceType].query(
+                                angular.extend(parameters[$scope.resourceType], newValue),
+                                function(data) {
 
-                            if (isValidInput(searchForm.title)) {
-                                search.title = searchForm.title;
-                            }
-                            if (isValidInput(searchForm.user)) {
-                                search['owner.id'] = searchForm.user.id;
-                            }
+                                    // The currentPage set should be before the
+                                    // resourceList set. This will allow the change
+                                    // in the resourceList to initiate a change
+                                    // in the page
+                                    $scope.resourcePages.currentPage = 1;
+                                    $scope.resourceList = _.sortBy(data, sortBy[$scope.resourceType]);
+                                });
+                    });
 
-                            $scope.searchFilters = search;
-                        };
-                    },
-                    link: function(scope, elem, attrs) {
-
-                        datastore.get('user', {}, function(users) {
-                            scope.users = users;
+                    // Gets all the resources that have the selected option set.
+                    function extractSelected(list) {
+                        return _.filter(list, function(resource) {
+                            return resource.selected;
                         });
                     }
-                };
-            }])
-        .directive('eventList', ['datastore', function(datastore) {
-                return {
-                    restrict: 'E',
-                    templateUrl: '/partials/eventlist.html',
-                    scope: {
-                        eventList: '='
-                    },
-                    controller: function($scope) {
-                        $scope.hello = "Welcome message";
-                    },
-                    link: function(scope, elem, attrs) {
-                        console.log('eventList');
+
+                    $scope.deleteSelected = function(list) {
+                        var deleteList = extractSelected(list);
+
+                        if (deleteList.length === 0) {
+                            return; // Do nothing if nothing is selected.
+                        }
+
+                        confirmDialog({
+                            message: "You're about to delete " + deleteList.length + " " + $scope.resourceType + "s. This is non-reversible.",
+                            confirm: function(confirmation) {
+                                if (confirmation === false) {
+                                    return; // do nothing
+                                }
+
+                                _.each(deleteList, function(item) {
+                                    api[$scope.resourceType].delete({id: item.id}, function() {
+                                        list.splice(_.indexOf(list, item), 1);
+                                    });
+                                });
+                            },
+                            cancel: function() {
+                            }
+                        })();
+                    };
+
+                    $scope.resourcePages = {
+                        itemsPerPage: 25,
+                        maxSize: 5, // The maximum number of page numbers to display.
+                        currentPage: 1
+                    };
+
+                    if ($scope.resourceType === 'dataset') {
+                        $scope.searchEvents = function(datasetList) {
+
+                            var selectedList = extractSelected(datasetList);
+                            if (selectedList.length === 0) {
+                                return; // Do nothing if nothing is selected
+                            }
+
+                            var url = '/event/?datasetId=' + _.pluck(selectedList, 'id').join(',');
+
+                            $location.url(url);
+                        };
                     }
-                };
-            }]);
+                }
+            };
+        })
+        .directive('datasetSearch', function(api) {
+            return {
+                restrict: 'E',
+                scope: {
+                    searchFilters: '='
+                },
+                templateUrl: '/partials/datasetsearch.html',
+                controller: function($scope, $location) {
+
+                    $scope.search = {};
+
+                    var queryParameters = $location.search();
+
+                    // On page load pull in the initial search parameters to
+                    // populate the form
+                    $scope.search.title = queryParameters.title;
+
+                    api.user.query({}, function(users) {
+                        $scope.users = users;
+
+                        // First time prepopulate
+                        if (queryParameters['owner.id']) {
+                            $scope.users.forEach(function(user) {
+                                if (user.id === queryParameters['owner.id']) {
+                                    $scope.search.user = user;
+                                }
+                            });
+                        }
+                    });
+
+                    $scope.formChange = function(searchForm) {
+
+                        // title, user, startTime, endTime
+                        function isValidInput(input) {
+                            return angular.isDefined(input)
+                                    && input !== null
+                                    && input !== ''
+                                    && input !== {};
+                        }
+
+                        var search = {};
+
+                        if (isValidInput(searchForm.title)) {
+                            search.title = searchForm.title;
+                        }
+                        if (isValidInput(searchForm.user)) {
+                            search['owner.id'] = searchForm.user.id;
+                        }
+
+                        $scope.searchFilters = search;
+
+                        $scope.search = searchForm; // Need this line to keep the form values populated
+                    };
+                }
+            };
+        });
--- development/js/singlepage/services.js
+++ development/js/singlepage/services.js
@@ -1,34 +1,63 @@
 'use strict';
 
-/* Services */
+angular.module('redApp.services', [])
 
+        /** Create a Red9 API accessor object
+         *  
+         *  Each resource has the following methods:
+         *   - save (create)
+         *   - get
+         *   - query
+         *   - update
+         *   - delete
+         *  
+         */
+        .factory('api', function($resource, $http, apiUrl) {
+            $http.defaults.withCredentials = true;
 
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', [])
-        .value('version', '0.1')
-        .factory('datastore', ['$http', 'apiUrl', function($http, apiUrl) {
-                return {
-                    get: function(resourceName, parameters, callback) {
-                        $http({
-                            method: 'GET',
-                            params: parameters,
-                            withCredentials: true,
-                            url: apiUrl + '/' + resourceName + '/'
-                        }).success(function(data, status) {
-                            callback(data);
-                        }).error(function(data, status) {
-                            console.log('Error!');
-                        });
-                    }
-                };
-            }])
-        .factory('api', function($resource, apiUrl) {
+            var apiOptions = {
+                update: {method: 'PUT'}
+            };
+
             return {
-                dataset: $resource(apiUrl + '/dataset/:id', {id: '@id'}),
-                event: $resource(apiUrl + '/event/:id', {id: '@id'}),
-                comment: $resource(apiUrl + '/comment/:id', {id: '@id'}),
-                user: $resource(apiUrl + '/user/:id', {id: '@id'}),
-                video: $resource(apiUrl + '/video/:id', {id: '@id'})
+                dataset: $resource(apiUrl + '/dataset/:id', {id: '@id'}, apiOptions),
+                event: $resource(apiUrl + '/event/:id', {id: '@id'}, apiOptions),
+                comment: $resource(apiUrl + '/comment/:id', {id: '@id'}, apiOptions),
+                user: $resource(apiUrl + '/user/:id', {id: '@id'}, apiOptions),
+                video: $resource(apiUrl + '/video/:id', {id: '@id'}, apiOptions)
+            };
+        })
+        .factory('confirmDialog', function($modal) {
+
+
+            return function(parameters) {
+                return function() {
+                    var ModalInstanceCtrl = function($scope, $modalInstance) {
+                        $scope.confirm = function() {
+                            $modalInstance.close(true);
+                        };
+
+                        $scope.cancel = function() {
+                            $modalInstance.dismiss('cancel');
+                        };
+                        $scope.message = parameters.message;
+                    };
+
+                    var modalInstance = $modal.open({
+                        templateUrl: '/partials/modals/areyousure.html',
+                        controller: ModalInstanceCtrl,
+                        size: 'sm'
+                    });
+
+                    modalInstance.result.then(parameters.confirm, parameters.cancel);
+
+                };
+                /*
+                 modalInstance.result.then(function(confirmed) {
+                 console.log('confirmed: ' + confirmed);
+                 }, function() {
+                 console.log('Modal dismissed at: ' + new Date());
+                 });
+                 */
             };
         });
