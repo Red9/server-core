cb37c6c6f60fe4976208d8ceb9d3110c77370e56
srlm <srlm@srlmproductions.com>
2014-08-26 22:11:52 -0400

    DW-219: fixed some issues with the FCPXML dialog. It wasn't doing the refs right for the text and Mike wanted to adjust the title duration.

--- static/js/tiles/embeddedvideo.fcpxmldialog.js
+++ static/js/tiles/embeddedvideo.fcpxmldialog.js
@@ -1,4 +1,4 @@
-define(['vendor/jquery', 'vendor/underscore', 'vendor/jquery.validate'], function($, _) {
+define(['vendor/jquery', 'vendor/underscore', 'vendor/jquery.validate'], function ($, _) {
     function fcpxmldialog(sandbox, tile, configuration, doneCallback) {
 
         var videoTypes = {
@@ -49,7 +49,7 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/jquery.validate'], functio
                 return Math.floor(value / numerator) * numerator;
             }
 
-            var assets = _.map(formValues.files, function(filename, index) {
+            var assets = _.map(formValues.files, function (filename, index) {
                 return {
                     ref: 'r' + (index + 3),
                     source: filename.replace(/ /g, '%20'),
@@ -62,29 +62,32 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/jquery.validate'], functio
             // is "rightmost"
             assets.reverse();
 
+            var titleReferenceCounter = 1;
+            var titleDuration = frameCorrected(formValues.titleDuration * denominator);
+
             var runningOffset = 0;
             var clips =
-                    _.chain(eventList)
+                _.chain(eventList)
                     // Only include events who meet the minimum staring requirements.
-                    .filter(function(event) {
+                    .filter(function (event) {
                         try {
                             // Make sure that we get exactly the event type ('Wave' vs 'wave' vs 'Wave: Right')
                             return (formValues.type === event.type || typeof formValues.tight === 'undefined') &&
-                                    (formValues.stars === 'any' || event.stars['acceleration:z'] >= formValues.stars);
+                                (formValues.stars === 'any' || event.stars['acceleration:z'] >= formValues.stars);
                         } catch (e) {
                             return false; // default to no accept.
                         }
                     })
-                    .sortBy(function(event) {
+                    .sortBy(function (event) {
                         return event.startTime;
                     })
-                    .map(function(event, index, list) {
+                    .map(function (event, index, list) {
                         var durationFrames = frameCorrected((event.endTime - event.startTime) / 1000 * denominator);
                         var startFramesTotal = frameCorrected((event.startTime - video.startTime) / 1000 * denominator);
 
                         // Find the asset. The list must be reversed for this
                         // to work correctly.
-                        var asset = _.find(assets, function(asset) {
+                        var asset = _.find(assets, function (asset) {
                             return asset.start <= startFramesTotal;
                         });
 
@@ -97,13 +100,18 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/jquery.validate'], functio
                         } catch (e) {
                         }
 
-                        var title = {
-                            name: event.type + ' ' + (index + 1) + ' of ' + list.length,
-                            textA: event.stars['acceleration:z'] + ' stars',
-                            textB: distance + ' meters',
-                            offset: startFrames,
-                            duration: '181181'
-                        };
+                        var title;
+                        if (titleDuration !== 0) {
+                            title = {
+                                name: event.type + ' ' + (index + 1) + ' of ' + list.length,
+                                textA: event.stars['acceleration:z'] + ' stars',
+                                textARef: 'ts' + (titleReferenceCounter++),
+                                textB: distance + ' meters',
+                                textBRef: 'ts' + (titleReferenceCounter++),
+                                offset: startFrames,
+                                duration: titleDuration
+                            };
+                        }
 
                         var result = {
                             name: event.type + ' ' + event.id,
@@ -141,23 +149,23 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/jquery.validate'], functio
 
             // Convert the HTML form into a key/value list.
             $form.find('input, select, textarea')
-                    .not('[name=submitButton]') // This is definately a hack... Couldn't get it to work otherwise.
-                    .not('[type=radio]')
-                    .not('[type=checkbox]')
-                    .each(function() {
-                        var $this = $(this);
-                        var key = $this.attr('name');
-                        var value = $this.val();
-                        formValues[key] = value;
-                    });
-            $form.find('input[type=radio]:checked').each(function() {
+                .not('[name=submitButton]') // This is definately a hack... Couldn't get it to work otherwise.
+                .not('[type=radio]')
+                .not('[type=checkbox]')
+                .each(function () {
+                    var $this = $(this);
+                    var key = $this.attr('name');
+                    var value = $this.val();
+                    formValues[key] = value;
+                });
+            $form.find('input[type=radio]:checked').each(function () {
                 var $this = $(this);
                 var key = $this.attr('name');
                 var value = $this.val();
                 formValues[key] = value;
             });
 
-            $form.find('input[type=checkbox]:checked').each(function() {
+            $form.find('input[type=checkbox]:checked').each(function () {
                 var $this = $(this);
                 var key = $this.attr('name');
                 var value = $this.val();
@@ -165,33 +173,33 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/jquery.validate'], functio
             });
 
             // Extract the filenames into an array.
-            formValues.files = _.map(formValues.files.split(','), function(file) {
+            formValues.files = _.map(formValues.files.split(','), function (file) {
                 return file.replace(/^\s+|\s+$/g, ''); // trim whitespace
             });
-            sandbox.requestTemplate('fcpxml', function(fcpxmlTemplate) {
-                sandbox.get('dataset', {id: formValues.datasetId}, function(datasetList) {
+            sandbox.requestTemplate('fcpxml', function (fcpxmlTemplate) {
+                sandbox.get('dataset', {id: formValues.datasetId}, function (datasetList) {
                     sandbox.get('event',
-                            {
-                                datasetId: formValues.datasetId,
-                                type: formValues.type
-                            }, function(eventList) {
-                        sandbox.get('video', {dataset: formValues.datasetId}, function(videoList) {
-                            // In case we have multiple associated videos: use the
-                            // first in time as our base truth.
-                            videoList = _.sortBy(videoList, function(video) {
-                                return video.startTime;
+                        {
+                            datasetId: formValues.datasetId,
+                            type: formValues.type
+                        }, function (eventList) {
+                            sandbox.get('video', {dataset: formValues.datasetId}, function (videoList) {
+                                // In case we have multiple associated videos: use the
+                                // first in time as our base truth.
+                                videoList = _.sortBy(videoList, function (video) {
+                                    return video.startTime;
+                                });
+                                outputFCPXML(formValues, fcpxmlTemplate, eventList, videoList[0], datasetList[0]);
                             });
-                            outputFCPXML(formValues, fcpxmlTemplate, eventList, videoList[0], datasetList[0]);
                         });
-                    });
                 });
             });
         }
 
         function showForm(place, configuration) {
-            sandbox.requestTemplate('embeddedvideo.fcpxmldialog', function(template) {
-                sandbox.get('eventtype', {}, function(eventTypes) {
-                    _.each(eventTypes, function(type) {
+            sandbox.requestTemplate('embeddedvideo.fcpxmldialog', function (template) {
+                sandbox.get('eventtype', {}, function (eventTypes) {
+                    _.each(eventTypes, function (type) {
                         if (type.name === 'Wave') {
                             type.selected = 'selected';
                         } else {
@@ -214,10 +222,10 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/jquery.validate'], functio
 
         function destructor() {
             sandbox
-                    = tile
-                    = configuration
-                    = doneCallback
-                    = null;
+                = tile
+                = configuration
+                = doneCallback
+                = null;
         }
 
         init();
@@ -225,5 +233,6 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/jquery.validate'], functio
             destructor: destructor
         };
     }
+
     return fcpxmldialog;
 });
